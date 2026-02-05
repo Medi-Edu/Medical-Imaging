bf90cc3254af18fc0eff5fc495fdb2eb40aa529c
Medi-Edu <zabirul.islam1996@gmail.com>
2026-02-05 10:46:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // CONFIG
 // ===============================
 // ✅ MUST be HTTPS when using GitHub Pages (HTTPS)
-const API_BASE = "https://establishing-assignment-investigations-favor.trycloudflare.com";
+const API_BASE = "https://geographic-harvest-spa-repair.trycloudflare.com";
 
 // Polling
 const POLL_MS = 1500;
